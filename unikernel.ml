bff1eccbfd05279976ebd4cf265e24f2dccab951
lcoviedo <psxlco@nottingham.ac.uk>
2015-06-02 11:32:37 +0100

    Update unikernel.ml

--- unikernel.ml
+++ unikernel.ml
@@ -11,22 +11,27 @@ let domain = "anil.recoil.org"
 let uri = Uri.of_string "http://anil.recoil.org"
 let ns = "8.8.8.8"
 
-module Client (C:CONSOLE) (RES:Resolver_lwt.S) (CON:Conduit_mirage.S) = struct
+module Client (C:CONSOLE) (S:STACKV4) = struct
 
   module HTTP = Cohttp_mirage.Client
-
-  let http_fetch c resolver ctx =
+  module DNS = Dns_resolver_mirage.Make(OS.Time)(S)
+  module RES = Resolver_mirage.Make(DNS)
+  
+  let http_fetch c ctx =
     C.log_s c (sprintf "Fetching %s with Cohttp:" (Uri.to_string uri)) >>= fun () ->
-    let ctx = HTTP.ctx resolver ctx in
     HTTP.get ~ctx uri >>= fun (response, body) ->
     Cohttp_lwt_body.to_string body >>= fun body ->
     C.log_s c (Sexplib.Sexp.to_string_hum (Cohttp.Response.sexp_of_t response)) >>= fun () ->
     C.log_s c (sprintf "Received body length: %d" (String.length body)) >>= fun () ->
     C.log_s c "Cohttp fetch done\n------------\n"
 
-  let start c res ctx =
+  let start c stack =
     C.log_s c (sprintf "Resolving in 1s using DNS server %s" ns) >>= fun () ->
+    let conduit = Conduit_mirage.with_tcp Conduit_mirage.empty (module S) stack in
+    let res = Resolver_lwt.init () in
+    RES.register ~ns:(Ipaddr.V4.of_string_exn ns) ~stack res;
+    let ctx = HTTP.ctx res conduit in
     OS.Time.sleep 1.0 >>= fun () ->
-    http_fetch c res ctx 
+    http_fetch c ctx 
     
 end
